b294cc5439c5961a7e5fc3087e5e715e95ccf0c1
Xxiled <93330009+xxiled-plastic-cat@users.noreply.github.com>
2024-06-03 12:34:23 +0100

    initial setup

---
+++ bc-1/projects/bc-1/__test__/DigitalMarketplace.test.ts
@@ -0,0 +1,49 @@
+import { describe, test, expect, beforeAll, beforeEach } from '@jest/globals';
+import { algorandFixture } from '@algorandfoundation/algokit-utils/testing';
+import * as algokit from '@algorandfoundation/algokit-utils';
+import { DigitalMarketplaceClient } from '../contracts/clients/DigitalMarketplaceClient';
+
+const fixture = algorandFixture();
+algokit.Config.configure({ populateAppCallResources: true });
+
+let appClient: DigitalMarketplaceClient;
+
+describe('DigitalMarketplace', () => {
+  beforeEach(fixture.beforeEach);
+
+  beforeAll(async () => {
+    await fixture.beforeEach();
+    const { testAccount } = fixture.context;
+    const { algorand } = fixture;
+
+    appClient = new DigitalMarketplaceClient(
+      {
+        sender: testAccount,
+        resolveBy: 'id',
+        id: 0,
+      },
+      algorand.client.algod
+    );
+
+    await appClient.create.createApplication({});
+  });
+
+  test('sum', async () => {
+    const a = 13;
+    const b = 37;
+    const sum = await appClient.doMath({ a, b, operation: 'sum' });
+    expect(sum.return?.valueOf()).toBe(BigInt(a + b));
+  });
+
+  test('difference', async () => {
+    const a = 13;
+    const b = 37;
+    const diff = await appClient.doMath({ a, b, operation: 'difference' });
+    expect(diff.return?.valueOf()).toBe(BigInt(a >= b ? a - b : b - a));
+  });
+
+  test('hello', async () => {
+    const diff = await appClient.hello({ name: 'world!' });
+    expect(diff.return?.valueOf()).toBe('Hello, world!');
+  });
+});
---
+++ bc-1/projects/bc-1/contracts/DigitalMarketplace.algo.ts
@@ -0,0 +1,62 @@
+import { Contract } from '@algorandfoundation/tealscript';
+
+export class DigitalMarketplace extends Contract {
+  assetId = GlobalStateKey<AssetID>();
+
+  unitaryPrice = GlobalStateKey<uint64>();
+
+  createApplication(assetId: AssetID, unitaryPrice: uint64): void {
+    this.assetId.value = assetId;
+    this.unitaryPrice.value = unitaryPrice;
+  }
+
+  setPrice(unitaryPrice: uint64): void {
+    assert(this.txn.sender === this.app.creator);
+    this.unitaryPrice.value = unitaryPrice;
+  }
+
+  optInToAsset(mbrTxn: PayTxn): void {
+    assert(this.txn.sender === this.app.address);
+    verifyPayTxn(mbrTxn, {
+      receiver: this.app.address,
+      amount: globals.minBalance + globals.assetOptInMinBalance,
+    });
+    sendAssetTransfer({
+      xferAsset: this.assetId.value,
+      assetAmount: 0,
+      assetReceiver: this.app.address,
+    });
+  }
+
+  buy(buyerTxn: PayTxn, quantity: uint64): void {
+    assert(this.assetId.value.id !== 0, 'AssetID not set');
+    assert(this.unitaryPrice.value !== 0, 'Unitary price not set');
+    verifyPayTxn(buyerTxn, {
+      sender: this.txn.sender,
+      receiver: this.app.address,
+      amount: this.unitaryPrice.value * quantity,
+    });
+    sendAssetTransfer({
+      xferAsset: this.assetId.value,
+      assetAmount: quantity,
+      assetReceiver: this.txn.sender,
+    });
+  }
+
+  deleteApplication(): void {
+    assert(this.txn.sender === this.app.creator);
+
+    sendAssetTransfer({
+      xferAsset: this.assetId.value,
+      assetReceiver: this.app.creator,
+      assetAmount: this.app.address.assetBalance(this.assetId.value),
+      assetCloseTo: this.app.creator,
+    });
+    sendPayment({
+
+      amount: this.app.address.balance,
+      receiver: this.app.creator,
+      closeRemainderTo: this.app.creator,
+    });
+  }
+}
